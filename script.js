6f78f641c6a129da44c095a4bdae23aa535a3b33
diasdiogo2842-create <diasdiogo2842@gmail.com>
2025-08-05 16:43:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-function extractCardIDs(link) {
-  try {
-    const url = new URL(link);
-    const deckParam = url.searchParams.get("deck");
-    if (!deckParam) return [];
-
-    const rawIDs = deckParam.split(";");
-    const validIDs = rawIDs.filter(id => id.startsWith("2600") && cardMap[id]);
-    return validIDs;
-  } catch (e) {
-    return [];
-  }
-}
+<!-- Replace originalDeck & improvedDeck divs -->
+<div class="deck-display" id="originalDeck"></div>
+<div class="result" id="resultText"></div>
+<div class="suggestion" id="swapSuggestions"></div>
+<div class="deck-display" id="improvedDeck"></div>
